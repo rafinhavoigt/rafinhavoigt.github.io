98a88b60f76a10560a63f6aebb2d87654cf86803
cizordj <32869222+cizordj@users.noreply.github.com>
2021-04-25 14:54:49 -0300

    Update the lowbar's appearance
    - Add some colors to the palette
    - Add a button which opens a modal

--- src/components/LowBar/index.js
+++ src/components/LowBar/index.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import {AppBar, Toolbar, Typography, Box} from '@material-ui/core';
+import {AppBar, Toolbar, Typography, Button} from '@material-ui/core';
 import AboutModal from '../AboutModal/index.js';
 import InfoIcon from '@material-ui/icons/Info';
 import {makeStyles} from '@material-ui/core/styles';
-import {color0, color1} from '../../styles/colors';
+import {color0, color1, color2, color3} from '../../styles/colors';
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
     top: 'auto',
     bottom: 0,
-    backgroundColor: color0
+    backgroundColor: color0,
+    color: color1
   },
   title: {
     paddingRight: '1rem'
   },
   about: {
+    marginLeft: 'auto',
+    backgroundColor: color2,
+    color: color1,
     '&:hover': {
       cursor: 'pointer',
-      color: color0,
-      backgroundColor: color1
+      backgroundColor: color3
     }
   }
 }));
@@ -37,25 +40,20 @@ export default function LowBar() {
 
   return (
     <>
-      <AppBar position="fixed" color="primary" className={classes.appBar}>
+      <AppBar position="fixed" className={classes.appBar}>
         <Toolbar>
           <Typography className={classes.title}>Rafinha Voigt</Typography>
-          <Typography variant="small" component="small">
+          <Typography variant="subtitle" component="small">
             Fotografias
           </Typography>
-          <Box
-            ml="auto"
-            justifyContent="center"
-            alignItems="center"
-            display="flex"
-            onClick={openModal}
+          <Button
             className={classes.about}
+            variant="contained"
+            endIcon={<InfoIcon />}
+            onClick={openModal}
           >
-            <Typography>Sobre</Typography>
-            <Box pl={1}>
-              <InfoIcon color="inherit" fontSize="small" />
-            </Box>
-          </Box>
+            Sobre
+          </Button>
         </Toolbar>
       </AppBar>
       <AboutModal state={open} callback={closeModal} />
--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,4 +1,6 @@
-export const color0 = '#000';
+export const color0 = '#424242';
 export const color1 = '#FFF';
-export const color2 = '#8da8be'; // Kelidoscope
-export const color3 = '#f6c700';
+export const color4 = '#8da8be'; // Kelidoscope
+export const color2 = '#0d47a1';
+export const color3 = '#3d6bb3';
+export const color5 = '#f6c700';
