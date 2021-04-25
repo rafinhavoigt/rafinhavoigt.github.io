f3b3429f8b2b3a0f656700bd0d651f774e99e6e5
cizordj <32869222+cizordj@users.noreply.github.com>
2021-04-25 14:58:44 -0300

    Update the colors palette

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,6 +1,6 @@
 export const color0 = '#424242';
 export const color1 = '#FFF';
-export const color4 = '#8da8be'; // Kelidoscope
-export const color2 = '#0d47a1';
-export const color3 = '#3d6bb3';
+export const color2 = '#8da8be'; // Kelidoscope
+export const color3 = '#0d47a1';
+export const color4 = '#3d6bb3';
 export const color5 = '#f6c700';
