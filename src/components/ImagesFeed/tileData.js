e36eab32437ef892435dc2a6a31be8ff51e61cb4
cizordj <32869222+cizordj@users.noreply.github.com>
2021-05-02 22:04:48 -0300

    Add an image grid to the site
    - Add a grid of images to the main page which displays all pictures
    chosen by the photographer

---
+++ src/components/ImagesFeed/tileData.js
@@ -0,0 +1,144 @@
+import image from '../../assets/img/big/1.webp';
+import image2 from '../../assets/img/big/2.webp';
+import image3 from '../../assets/img/big/3.webp';
+import image4 from '../../assets/img/big/4.webp';
+import image5 from '../../assets/img/big/5.webp';
+import image6 from '../../assets/img/big/6.webp';
+import image7 from '../../assets/img/big/7.webp';
+import image8 from '../../assets/img/big/8.webp';
+import image9 from '../../assets/img/big/9.webp';
+import image10 from '../../assets/img/big/10.webp';
+import image11 from '../../assets/img/big/11.webp';
+import image12 from '../../assets/img/big/12.webp';
+import image13 from '../../assets/img/big/13.webp';
+import image14 from '../../assets/img/big/14.webp';
+import image15 from '../../assets/img/big/15.webp';
+import image16 from '../../assets/img/big/16.webp';
+import image17 from '../../assets/img/big/17.webp';
+import image18 from '../../assets/img/big/18.webp';
+import image19 from '../../assets/img/big/19.webp';
+import image20 from '../../assets/img/big/20.webp';
+
+const tileData = [
+  {
+    img: image,
+    title: 'Image',
+    author: 'author',
+    cols: 1
+  },
+  {
+    img: image2,
+    title: 'Image',
+    author: 'author',
+    cols: 1
+  },
+  {
+    img: image3,
+    title: 'Image',
+    author: 'author',
+    cols: 1
+  },
+  {
+    img: image4,
+    title: 'Image',
+    author: 'author',
+    cols: 1
+  },
+  {
+    img: image5,
+    title: 'Image',
+    author: 'author',
+    cols: 1
+  },
+  {
+    img: image6,
+    title: 'Image',
+    author: 'author',
+    cols: 1
+  },
+  {
+    img: image7,
+    title: 'Image',
+    author: 'author',
+    cols: 1
+  },
+  {
+    img: image8,
+    title: 'Image',
+    author: 'author',
+    cols: 1
+  },
+  {
+    img: image9,
+    title: 'Image',
+    author: 'author',
+    cols: 1
+  },
+  {
+    img: image10,
+    title: 'Image',
+    author: 'author',
+    cols: 1
+  },
+  {
+    img: image11,
+    title: 'Image',
+    author: 'author',
+    cols: 1
+  },
+  {
+    img: image12,
+    title: 'Image',
+    author: 'author',
+    cols: 1
+  },
+  {
+    img: image13,
+    title: 'Image',
+    author: 'author',
+    cols: 1
+  },
+  {
+    img: image14,
+    title: 'Image',
+    author: 'author',
+    cols: 1
+  },
+  {
+    img: image15,
+    title: 'Image',
+    author: 'author',
+    cols: 1
+  },
+  {
+    img: image16,
+    title: 'Image',
+    author: 'author',
+    cols: 1
+  },
+  {
+    img: image17,
+    title: 'Image',
+    author: 'author',
+    cols: 1
+  },
+{
+    img: image18,
+    title: 'Image',
+    author: 'author',
+    cols: 1
+  },
+  {
+    img: image19,
+    title: 'Image',
+    author: 'author',
+    cols: 1
+  },
+  {
+    img: image20,
+    title: 'Image',
+    author: 'author',
+    cols: 1
+  }
+];
+export { tileData };
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../../components/Header';
+import ImagesFeed from '../../components/ImagesFeed';
 import Footer from '../../components/Footer';
 
 export default function Home() {
   return (
     <>
       <Header />
+      <ImagesFeed />
       <Footer />
     </>
   );
