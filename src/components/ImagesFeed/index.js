6f9897e22fceade6d95ab7c8c7ea7f9b4de78f1f
cizordj <32869222+cizordj@users.noreply.github.com>
2021-05-02 23:34:48 -0300

    Add dynamic number of columns
    The number of columns will adjust itself automatically depending on the
    screen size, that is, it will fit the device's screen forever

--- src/components/ImagesFeed/index.js
+++ src/components/ImagesFeed/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
+import {useTheme} from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
-import { tileData } from './tileData';
+import {tileData} from './tileData';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -11,18 +13,31 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: 'space-around',
     overflow: 'hidden',
     backgroundColor: theme.palette.background.paper
-  },
-  gridList: {
-    width: 500,
-    height: 450
   }
 }));
 
 export default function ImagesFeed() {
+  const theme = useTheme();
   const classes = useStyles();
+  const [columns, setColumns] = React.useState(0);
+  const isMobileDevice = useMediaQuery(theme.breakpoints.down('xs'));
+  const isTablet = useMediaQuery(theme.breakpoints.between('sm', '1200'));
+
+  React.useEffect(() => {
+    console.log('isMobile ' + isMobileDevice);
+    console.log('isTablet ' + isTablet);
+    if (isMobileDevice === true) {
+      setColumns(2);
+    } else if (isTablet === true) {
+      setColumns(3);
+    } else {
+      setColumns(5);
+    }
+  }, [isMobileDevice, isTablet]);
+
   return (
     <div className={classes.root}>
-      <GridList cellHeight={160} className={classes.gridList} cols={2}>
+      <GridList cellHeight={160} cols={columns}>
         {tileData.map((tile) => (
           <GridListTile key={tile.img} cols={tile.cols || 1}>
             <img src={tile.img} alt={tile.title} />
@@ -31,4 +46,4 @@ export default function ImagesFeed() {
       </GridList>
     </div>
   );
-};
+}
