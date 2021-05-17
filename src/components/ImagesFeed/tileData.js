0cf612c44fd83dfb8c108a2acdb3d8f94817e789
cizordj <32869222+cizordj@users.noreply.github.com>
2021-05-16 21:10:14 -0300

    Update the reference to all pictures

--- src/components/ImagesFeed/tileData.js
+++ src/components/ImagesFeed/tileData.js
@@ -1,142 +1,182 @@
-import image from '../../assets/img/thumbnail/1.webp';
-import image2 from '../../assets/img/thumbnail/2.webp';
-import image3 from '../../assets/img/thumbnail/3.webp';
-import image4 from '../../assets/img/thumbnail/4.webp';
-import image5 from '../../assets/img/thumbnail/5.webp';
-import image6 from '../../assets/img/thumbnail/6.webp';
-import image7 from '../../assets/img/thumbnail/7.webp';
-import image8 from '../../assets/img/thumbnail/8.webp';
-import image9 from '../../assets/img/thumbnail/9.webp';
-import image10 from '../../assets/img/thumbnail/10.webp';
-import image11 from '../../assets/img/thumbnail/11.webp';
-import image12 from '../../assets/img/thumbnail/12.webp';
-import image13 from '../../assets/img/thumbnail/13.webp';
-import image14 from '../../assets/img/thumbnail/14.webp';
-import image15 from '../../assets/img/thumbnail/15.webp';
-import image16 from '../../assets/img/thumbnail/16.webp';
-import image17 from '../../assets/img/thumbnail/17.webp';
-import image18 from '../../assets/img/thumbnail/18.webp';
-import image19 from '../../assets/img/thumbnail/19.webp';
-import image20 from '../../assets/img/thumbnail/20.webp';
+import thumbnail1 from '../../assets/img/thumbnail/1.webp';
+import thumbnail2 from '../../assets/img/thumbnail/2.webp';
+import thumbnail3 from '../../assets/img/thumbnail/3.webp';
+import thumbnail4 from '../../assets/img/thumbnail/4.webp';
+import thumbnail5 from '../../assets/img/thumbnail/5.webp';
+import thumbnail6 from '../../assets/img/thumbnail/6.webp';
+import thumbnail7 from '../../assets/img/thumbnail/7.webp';
+import thumbnail8 from '../../assets/img/thumbnail/8.webp';
+import thumbnail9 from '../../assets/img/thumbnail/9.webp';
+import thumbnail10 from '../../assets/img/thumbnail/10.webp';
+import thumbnail11 from '../../assets/img/thumbnail/11.webp';
+import thumbnail12 from '../../assets/img/thumbnail/12.webp';
+import thumbnail13 from '../../assets/img/thumbnail/13.webp';
+import thumbnail14 from '../../assets/img/thumbnail/14.webp';
+import thumbnail15 from '../../assets/img/thumbnail/15.webp';
+import thumbnail16 from '../../assets/img/thumbnail/16.webp';
+import thumbnail17 from '../../assets/img/thumbnail/17.webp';
+import thumbnail18 from '../../assets/img/thumbnail/18.webp';
+import thumbnail19 from '../../assets/img/thumbnail/19.webp';
+import thumbnail20 from '../../assets/img/thumbnail/20.webp';
+import image1 from '../../assets/img/big/1.webp';
+import image2 from '../../assets/img/big/2.webp';
+import image3 from '../../assets/img/big/3.webp';
+import image4 from '../../assets/img/big/4.webp';
+import image5 from '../../assets/img/big/5.webp';
+import image6 from '../../assets/img/big/6.webp';
+import image7 from '../../assets/img/big/7.webp';
+import image8 from '../../assets/img/big/8.webp';
+import image9 from '../../assets/img/big/9.webp';
+import image10 from '../../assets/img/big/10.webp';
+import image11 from '../../assets/img/big/11.webp';
+import image12 from '../../assets/img/big/12.webp';
+import image13 from '../../assets/img/big/13.webp';
+import image14 from '../../assets/img/big/14.webp';
+import image15 from '../../assets/img/big/15.webp';
+import image16 from '../../assets/img/big/16.webp';
+import image17 from '../../assets/img/big/17.webp';
+import image18 from '../../assets/img/big/18.webp';
+import image19 from '../../assets/img/big/19.webp';
+import image20 from '../../assets/img/big/20.webp';
 
 const tileData = [
   {
-    img: image,
-    title: 'Image',
+    thumbnail: thumbnail1,
+    image: image1,
+    title: 'thumbnail',
     author: 'author',
     cols: 1
   },
   {
-    img: image2,
-    title: 'Image',
+    thumbnail: thumbnail2,
+    image: image2,
+    title: 'thumbnail',
     author: 'author',
     cols: 1
   },
   {
-    img: image3,
-    title: 'Image',
+    thumbnail: thumbnail3,
+    image: image3,
+    title: 'thumbnail',
     author: 'author',
     cols: 1
   },
   {
-    img: image4,
-    title: 'Image',
+    thumbnail: thumbnail4,
+    image: image4,
+    title: 'thumbnail',
     author: 'author',
     cols: 1
   },
   {
-    img: image5,
-    title: 'Image',
+    thumbnail: thumbnail5,
+    image: image5,
+    title: 'thumbnail',
     author: 'author',
     cols: 1
   },
   {
-    img: image6,
-    title: 'Image',
+    thumbnail: thumbnail6,
+    image: image6,
+    title: 'thumbnail',
     author: 'author',
     cols: 1
   },
   {
-    img: image7,
-    title: 'Image',
+    thumbnail: thumbnail7,
+    image: image7,
+    title: 'thumbnail',
     author: 'author',
     cols: 1
   },
   {
-    img: image8,
-    title: 'Image',
+    thumbnail: thumbnail8,
+    image: image8,
+    title: 'thumbnail',
     author: 'author',
     cols: 1
   },
   {
-    img: image9,
-    title: 'Image',
+    thumbnail: thumbnail9,
+    image: image9,
+    title: 'thumbnail',
     author: 'author',
     cols: 1
   },
   {
-    img: image10,
-    title: 'Image',
+    thumbnail: thumbnail10,
+    image: image10,
+    title: 'thumbnail',
     author: 'author',
     cols: 1
   },
   {
-    img: image11,
-    title: 'Image',
+    thumbnail: thumbnail11,
+    image: image11,
+    title: 'thumbnail',
     author: 'author',
     cols: 1
   },
   {
-    img: image12,
-    title: 'Image',
+    thumbnail: thumbnail12,
+    image: image12,
+    title: 'thumbnail',
     author: 'author',
     cols: 1
   },
   {
-    img: image13,
-    title: 'Image',
+    thumbnail: thumbnail13,
+    image: image13,
+    title: 'thumbnail',
     author: 'author',
     cols: 1
   },
   {
-    img: image14,
-    title: 'Image',
+    thumbnail: thumbnail14,
+    image: image14,
+    title: 'thumbnail',
     author: 'author',
     cols: 1
   },
   {
-    img: image15,
-    title: 'Image',
+    thumbnail: thumbnail15,
+    image: image15,
+    title: 'thumbnail',
     author: 'author',
     cols: 1
   },
   {
-    img: image16,
-    title: 'Image',
+    thumbnail: thumbnail16,
+    image: image16,
+    title: 'thumbnail',
     author: 'author',
     cols: 1
   },
   {
-    img: image17,
-    title: 'Image',
+    thumbnail: thumbnail17,
+    image: image17,
+    title: 'thumbnail',
     author: 'author',
     cols: 1
   },
   {
-    img: image18,
-    title: 'Image',
+    thumbnail: thumbnail18,
+    image: image18,
+    title: 'thumbnail',
     author: 'author',
     cols: 1
   },
   {
-    img: image19,
-    title: 'Image',
+    thumbnail: thumbnail19,
+    image: image19,
+    title: 'thumbnail',
     author: 'author',
     cols: 1
   },
   {
-    img: image20,
-    title: 'Image',
+    thumbnail: thumbnail20,
+    image: image20,
+    title: 'thumbnail',
     author: 'author',
     cols: 1
   }
