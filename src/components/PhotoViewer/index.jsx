01efdf2224c1faf5392e2bae1c172056e0449ee7
cizordj <32869222+cizordj@users.noreply.github.com>
2021-05-17 22:16:35 -0300

    Update the photoviewer
    - Add some classes to make it transparent
    - Resize the picture to fit on the screen
    - Add a little bar at the top with a button to close the image viewer

--- src/components/PhotoViewer/index.jsx
+++ src/components/PhotoViewer/index.jsx
@@ -1,21 +1,75 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {makeStyles} from '@material-ui/core/styles';
 import Dialog from '@material-ui/core/Dialog';
 import Slide from '@material-ui/core/Slide';
+import DialogContent from '@material-ui/core/DialogContent';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
-export default function PhotoViewer({currentState, picture, callback}) {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: 'black'
+  },
+  dialogTitle: {
+    position: 'relative',
+    backgroundColor: 'transparent',
+    color: 'black'
+  },
+  dialogContent: {
+    backgroundColor: 'transparent',
+    padding: 0,
+    width: '100%'
+  },
+  closeButton: {
+    marginLeft: 'auto'
+  },
+  img: {
+    height: 512
+  }
+}));
+
+export default function PhotoViewer({currentState, picture, callback, title}) {
+  const classes = useStyles();
   return (
     <Dialog
-      fullScreen
+      fullscreen
       open={currentState}
       onClose={callback}
       TransitionComponent={Transition}
+      className={classes.root}
     >
-      <img src={picture} alt="Teste" onClick={callback} />
+      <AppBar className={classes.dialogTitle}>
+        <Toolbar>
+          <Typography variant="h6">{title}</Typography>
+          <IconButton
+            color="inherit"
+            onClick={callback}
+            aria-label="close"
+            className={classes.closeButton}
+          >
+            <CloseIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <DialogContent className={classes.dialogContent}>
+        <Box justifyContent="center" display="flex">
+          <img
+            src={picture}
+            alt={title}
+            onClick={callback}
+            className={classes.img}
+          />
+        </Box>
+      </DialogContent>
     </Dialog>
   );
 }
@@ -23,5 +77,6 @@ export default function PhotoViewer({currentState, picture, callback}) {
 PhotoViewer.propTypes = {
   currentState: PropTypes.bool,
   picture: PropTypes.string,
-  callback: PropTypes.func
+  callback: PropTypes.func,
+  title: PropTypes.string
 };
