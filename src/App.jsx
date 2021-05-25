8e9fc765c3ac4662a36c13c667887e4b7426ea26
cizordj <32869222+cizordj@users.noreply.github.com>
2021-05-24 21:07:13 -0300

    Create the dark mode for the whole website
    - Create the dark mode based on the documentation

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,29 @@ import React from 'react';
 import Header from './components/Header';
 import ImagesFeed from './components/ImagesFeed';
 import Footer from './components/Footer';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import {createMuiTheme, ThemeProvider} from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 function App() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: prefersDarkMode ? 'dark' : 'light'
+        }
+      }),
+    [prefersDarkMode]
+  );
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Header />
       <ImagesFeed />
       <Footer />
-    </>
+    </ThemeProvider>
   );
 }
 
